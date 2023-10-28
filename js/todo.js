d3a1401cd979383f111229e697f05fc378a5683d
Dmitry <128161876+Nekhocheninov@users.noreply.github.com>
2023-10-28 22:28:32 +0300

    Add files via upload

--- js/todo.js
+++ js/todo.js
@@ -1,16 +1,40 @@
 var input = document.querySelector("input[type = 'text']");
 var imgs = document.getElementsByTagName("img");
 
-function deleteTodo(){
+var lists = document.querySelectorAll(".list");
+
+// Add drag and drop functionality
+lists.forEach(function (list) {
+  list.addEventListener("dragover", function (event){
+    event.preventDefault();
+  });
+
+  list.addEventListener("drop", function (event){
+    event.preventDefault();
+    if (draggedItem) {
+      list.appendChild(draggedItem);
+      draggedItem = null;
+    }
+  });
+});
+
+var draggedItem = null;
+
+document.addEventListener("dragstart", function (event){
+  if (event.target.classList.contains("list")) return;
+  draggedItem = event.target;
+});
+
+function Todo(){
   for(let img of imgs){
     if (img.className == "trash"){
-      img.addEventListener ("click",function (){
+      img.addEventListener ("click", function (){
         img.parentElement.remove();
         event.stopImmediatePropagation();
       });
     }
     if (img.className == "img-right"){
-      img.addEventListener ("click",function (){
+      img.addEventListener ("click", function (){
         var node = img.parentNode;
         if (node.parentNode.id == "list1") addScrum('list2', node);
         else addScrum('list3', node);
@@ -18,7 +42,7 @@ function deleteTodo(){
       });
     }
     if (img.className == "img-left"){
-      img.addEventListener ("click",function (){
+      img.addEventListener ("click", function (){
         var node = img.parentNode;
         if (node.parentNode.id == "list3") addScrum('list2', node);
         else addScrum('list1', node);
@@ -41,8 +65,8 @@ input.addEventListener("keypress", function(keyPressed){
     clone.style = '';
     node.appendChild(clone).append(newTodo);
     this.value = "" ;
-    deleteTodo();
+    Todo();
   }
 });
 
-deleteTodo();
+Todo();
