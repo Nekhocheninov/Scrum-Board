69d127b674570778592bb645b4eac0b34985b3d2
Dmitry <128161876+Nekhocheninov@users.noreply.github.com>
2023-11-21 22:02:53 +0300

    Add files via upload

--- js/todo.js
+++ js/todo.js
@@ -13,26 +13,26 @@ const getNextElement = (cursorPosition, currentElement) => {
   return nextElement;
 };
 
-lists.forEach(function (list){
-  list.addEventListener("click", function (event){
+lists.forEach(function (list) {
+  list.addEventListener("click", function (event) {
     if (!(event.target.tagName === "LI")) return;
     event.target.classList.remove("editmode")
     event.target.getElementsByTagName("span")[0].setAttribute("contenteditable", "false");
     event.target.classList.contains("closed") ? event.target.classList.remove("closed") : event.target.classList.add("closed");
   });
 
-  list.addEventListener("dragend", function (event){
+  list.addEventListener("dragend", function (event) {
     event.target.classList.remove("selected");
     if (draggedItem) draggedItem = null;
   });
 
-  list.addEventListener("dragover", function (event){
+  list.addEventListener("dragover", function (event) {
     event.preventDefault();
     if (!draggedItem) return;
 
     const currentElement = event.target;
 
-    if (currentElement.tagName === "LI" && draggedItem !== currentElement){
+    if (currentElement.tagName === "LI" && draggedItem !== currentElement) {
       const activeElement = document.querySelector(`.selected`);
       const nextElement = getNextElement(event.clientY, currentElement);
       if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) return;
@@ -45,40 +45,40 @@ lists.forEach(function (list){
   });
 });
 
-document.addEventListener("dragstart", function (event){
+document.addEventListener("dragstart", function (event) {
   if (event.target.tagName !== "LI") return;
   event.target.classList.add("selected");
   draggedItem = event.target;
 });
 
-function Todo(){
-  for(let img of imgs){
-    if (img.className == "trash"){
-      img.addEventListener ("click", function (){
+function Todo() {
+  for(let img of imgs) {
+    if (img.className == "trash") {
+      img.addEventListener ("click", function () {
         img.parentElement.remove();
         event.stopImmediatePropagation();
       });
     }
-    if (img.className == "img-right"){
-      img.addEventListener ("click", function (){
+    if (img.className == "img-right") {
+      img.addEventListener ("click", function () {
         var node = img.parentNode;
         if (node.parentNode.id == "list1") addScrum('list2', node);
         else addScrum('list3', node);
         event.stopImmediatePropagation();
       });
     }
-    if (img.className == "img-left"){
-      img.addEventListener ("click", function (){
+    if (img.className == "img-left") {
+      img.addEventListener ("click", function () {
         var node = img.parentNode;
         if (node.parentNode.id == "list3") addScrum('list2', node);
         else addScrum('list1', node);
         event.stopImmediatePropagation();
       });
     }
-    if (img.className == "edit"){
-      img.addEventListener ("click", function (){
+    if (img.className == "edit") {
+      img.addEventListener ("click", function () {
         var span= img.parentElement.getElementsByTagName("span")[0];
-        if (span.getAttribute("contenteditable") === "false" || !span.hasAttribute("contenteditable")){
+        if (span.getAttribute("contenteditable") === "false" || !span.hasAttribute("contenteditable")) {
           span.setAttribute("contenteditable", "true");
           const range = document.createRange();
           range.selectNodeContents(span);
@@ -100,24 +100,44 @@ function Todo(){
   }
 }
 
-function addScrum(list, childe){
+function addScrum(list, childe) {
   const node = document.getElementById(list);
   node.appendChild(childe);
 }
 
-input.addEventListener("keypress", function(keyPressed){
-  if((keyPressed.which === 13) && (this.value != "")){
+input.addEventListener("keypress", function(keyPressed) {
+  if((keyPressed.which === 13) && (this.value != "")) {
     var newTodo = this.value;
     const node = document.getElementById('list1');
     var clone = node.querySelector('li').cloneNode(true);
     clone.style = '';
     clone.insertBefore(document.createTextNode(newTodo), clone.firstChild);
     var currentDate = new Date();
-    clone.querySelector('.DataTime').appendChild(document.createTextNode(currentDate.toLocaleString('ru-RU')));
+    clone.querySelector('.DateTime').appendChild(document.createTextNode(currentDate.toLocaleString('ru-RU')));
     node.appendChild(clone);
     this.value = "" ;
     Todo();
+    Scrum();
   }
 });
+
+function Scrum() {
+  getTasks(0)
+    .then(tasksArray => {
+      console.log('Массив задач пользователя:', tasksArray);
+      lists = ['list1', 'list2', 'list3']
+      const node = document.getElementById('list1');
+      for (let element of tasksArray) {
+        var clone = node.querySelector('li').cloneNode(true);
+        clone.style = '';
+        clone.insertBefore(document.createTextNode(element['title']), clone.firstChild);
+        clone.querySelector('.DateTime').appendChild(document.createTextNode(element['datetime']));
+        node.appendChild(clone);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
 
 Todo();
